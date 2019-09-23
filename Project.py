4f377a5498b21a243f0fc802fa5a0f5433e99316
Nizisi <54412715+Nizisi@users.noreply.github.com>
2019-09-23 10:53:16 -0400

    Add files via upload

--- Project.py
+++ Project.py
@@ -1,5 +1,8 @@
 #JieZhou
 #Future Project
 #A small game help me get more use to python
+#Python game based on python knowledge
+#1. look for enough info as base for the game (still collecting)
+#2. come up with questions (on process)
 print("Welcome to my new project")
 
