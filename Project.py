e377384664d706d811eef3055a89d76ecf264630
Nizisi <54412715+Nizisi@users.noreply.github.com>
2019-09-30 04:01:07 -0400

    Update Project.py

--- Project.py
+++ Project.py
@@ -5,4 +5,65 @@
 #1. look for enough info as base for the game (still collecting)
 #2. come up with questions (on process)
 print("Welcome to my new project")
-
+# sample start.
+print('--------Python study note---------')
+temp = input("Which year is qing dynasty：")
+guess = int(temp)
+if guess == 1636:
+    print("right")
+    print("nice job")
+else:
+    print("wrong")
+print("study harder")
+#sample for the question game
+#how to make a question base
+# use elif to go back to the start？
+#use def to go back
+# test
+print('--------Python study note---------')
+def 1():
+  temp = input("Which year is qing dynasty：")
+  guess = int(temp)
+  if guess == 1636:
+      print("right")
+      print("nice job")
+  else:
+      print("wrong")
+  print("study harder")
+Question = input(" pick one question : ")
+Qnum= int(Question)
+if Qnum== 1:
+  print(1)
+# cant use 1, use one instead
+print('--------Python study note---------')
+print(" question: 1
+def one():
+  temp = input("Which year is qing dynasty：")
+  guess = int(temp)
+  if guess == 1636:
+      print("right")
+      print("nice job")
+  else:
+      print("wrong")
+  print("study harder")
+Question = input(" pick one question : ")
+Qnum= int(Question)
+if Qnum== 1:
+  one()
+  # works well, trying to find a way to add menu, besides, analyzing question base
+  #test menu
+  print('--------Python study note---------')
+  print("Question: 1,......")
+def one():
+  temp = input("Which year is qing dynasty：")
+  guess = int(temp)
+  if guess == 1636:
+      print("right")
+      print("nice job")
+  else:
+      print("wrong")
+  print("study harder")
+Question = input(" pick one question : ")
+Qnum= int(Question)
+if Qnum== 1:
+  one()
